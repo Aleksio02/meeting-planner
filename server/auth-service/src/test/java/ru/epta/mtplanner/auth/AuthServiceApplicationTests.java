b9df916338bce841d8ef88e8bfa1b34dad70602d
Aleksio02 <oleks1o@yandex.ru>
2025-06-28 13:15:42 +0300

    [TASK-002] Created multimodule server project

---
+++ server/auth-service/src/test/java/ru/epta/mtplanner/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.epta.mtplanner.auth;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Disabled
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ server/meeting-service/src/test/java/ru/epta/mtplanner/meeting/MeetingServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.epta.mtplanner.meeting;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingServiceApplicationTests {
+
+	@Disabled
+	@Test
+	void contextLoads() {
+	}
+
+}

4951a1047663ed73d2c092e1aa3e44da5447441f
Aresik8 <romanisch08@gmail.com>
2025-07-02 23:14:08 +0300

    [TASK-015] создал сущность Invite

--- server/meeting-service/src/main/java/ru/epta/mtplanner/meeting/model/Invite.java
+++ server/meeting-service/src/main/java/ru/epta/mtplanner/meeting/model/Invite.java
@@ -1,14 +1,18 @@
 package ru.epta.mtplanner.meeting.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import ru.epta.commons.model.User;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Invite {
     private UUID id;
-    private Meeting mId;
-    private User uId;
+    private Meeting meetingId;
+    private User userId;
     private String status;
     private LocalDateTime sentAt;
 }

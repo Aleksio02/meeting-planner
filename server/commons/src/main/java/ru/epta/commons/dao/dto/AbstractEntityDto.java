489a8e2059b538a9dc653dc42699e65e5f6feef7
kizilovaas <kizilovaas001@gmail.com>
2025-07-02 21:26:05 +0300

    TASK-008 Создала AbstractEntityDto и UserDto

---
+++ server/commons/src/main/java/ru/epta/commons/dao/dto/AbstractEntityDto.java
@@ -0,0 +1,18 @@
+package ru.epta.commons.dao.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class AbstractEntityDto {
+    @Id
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+}

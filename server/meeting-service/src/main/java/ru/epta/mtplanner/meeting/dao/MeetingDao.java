7cf07e946fb4b9b31aaa2240b818aa44432851c8
Aleksio02 <oleks1o@yandex.ru>
2025-07-06 23:01:35 +0300

    [TASK-016] Реализовал получения списка событий по полям

--- server/meeting-service/src/main/java/ru/epta/mtplanner/meeting/dao/MeetingDao.java
+++ server/meeting-service/src/main/java/ru/epta/mtplanner/meeting/dao/MeetingDao.java
@@ -1,6 +1,7 @@
 package ru.epta.mtplanner.meeting.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import ru.epta.mtplanner.meeting.dao.dto.MeetingDto;
 
@@ -8,6 +9,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface MeetingDao extends JpaRepository<MeetingDto, UUID> {
+public interface MeetingDao extends JpaRepository<MeetingDto, UUID>, JpaSpecificationExecutor<MeetingDto> {
     Optional<MeetingDto> findByTitle(String title);
 }
--- server/meeting-service/src/main/java/ru/epta/mtplanner/meeting/model/Meeting.java
+++ server/meeting-service/src/main/java/ru/epta/mtplanner/meeting/model/Meeting.java
@@ -1,10 +1,16 @@
 package ru.epta.mtplanner.meeting.model;
+
+import lombok.Getter;
+import lombok.Setter;
 import ru.epta.commons.model.User;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Meeting {
+
     private UUID id;
     private String title;
     private String description;

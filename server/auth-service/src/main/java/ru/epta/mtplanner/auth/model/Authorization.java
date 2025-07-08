37c13cd8e36f1d44410f74f2f9c55dabe8f9d2ce
Aresik8 <romanisch08@gmail.com>
2025-07-08 19:34:55 +0300

    [TASK-011] создал сущность Authorization

---
+++ server/auth-service/src/main/java/ru/epta/mtplanner/auth/model/Authorization.java
@@ -0,0 +1,11 @@
+package ru.epta.mtplanner.auth.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Authorization {
+    private String login;
+    private String password;
+}

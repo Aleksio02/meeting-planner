1a5c9c26439f52dd14694ca839ee2074da68219c
kizilovaas <kizilovaas001@gmail.com>
2025-07-08 15:51:50 +0300

    TASK-018 Создала глобальный обработчик исключений

--- server/commons/src/main/java/ru/epta/commons/exception/GlobalControllerExceptionHandler.java
+++ server/commons/src/main/java/ru/epta/commons/exception/GlobalControllerExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import ru.epta.commons.model.ErrorResponse;
+import ru.epta.commons.model.response.ErrorResponse;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {

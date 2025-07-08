1c062447721878d9bf8e3bee36aa9a0a2cc278ee
1mperium <vanyalisenko1985@gmail.com>
2025-07-08 22:15:13 +0300

    TASK-024 | Сверстать форму добавления события

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,12 +1,14 @@
-import { Routes, Route, Navigate} from 'react-router-dom';
-import RegisterPage from '../pages/RegisterPage';
-import LoginPage from '../pages/LoginPage';
+import { Routes, Route, Navigate } from "react-router-dom";
+import RegisterPage from "../pages/RegisterPage";
+import LoginPage from "../pages/LoginPage";
+import HomePage from "../pages/HomePage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/register" />} />
-      <Route path="/login" element= {<LoginPage />}/> 
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );

4f38c837d58745fff84b88f0e380ec01651569d8
Aleksio02 <oleks1o@yandex.ru>
2025-07-11 21:54:08 +0300

    [TASK-035] Подключил либу для работы с почтой, добавил метод для отправления кода на почту (он асинхронный, чтобы респонс отдавался без задержек)

--- server/auth-service/src/main/java/ru/epta/mtplanner/auth/AuthServiceApplication.java
+++ server/auth-service/src/main/java/ru/epta/mtplanner/auth/AuthServiceApplication.java
@@ -5,15 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EntityScan({"ru.epta"})
 @EnableJpaRepositories("ru.epta")
 @ComponentScan("ru.epta")
 @SpringBootApplication
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
--- server/auth-service/src/main/java/ru/epta/mtplanner/auth/service/AuthServiceImpl.java
+++ server/auth-service/src/main/java/ru/epta/mtplanner/auth/service/AuthServiceImpl.java
@@ -1,28 +1,35 @@
 package ru.epta.mtplanner.auth.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import ru.epta.mtplanner.auth.hash.PasswordEncoder;
+import ru.epta.mtplanner.auth.model.request.Authorization;
+import ru.epta.mtplanner.auth.model.response.AuthResponse;
+import ru.epta.mtplanner.auth.utils.JwtUtils;
 import ru.epta.mtplanner.commons.converter.UserConverter;
 import ru.epta.mtplanner.commons.dao.UserDao;
 import ru.epta.mtplanner.commons.dao.dto.UserDto;
 import ru.epta.mtplanner.commons.exception.UnauthorizedException;
 import ru.epta.mtplanner.commons.model.User;
-import ru.epta.mtplanner.auth.hash.PasswordEncoder;
-import ru.epta.mtplanner.auth.utils.JwtUtils;
-import ru.epta.mtplanner.auth.model.request.Authorization;
-import ru.epta.mtplanner.auth.model.response.AuthResponse;
 
 @Primary
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    @Value("${spring.mail.username}")
+    private String SOURCE_EMAIL;
     private final UserDao userDao;
-
     private final JwtUtils jwtUtils;
+    private final JavaMailSender mailSender;
 
-    public AuthServiceImpl(UserDao userDao, JwtUtils jwtUtils) {
+    public AuthServiceImpl(UserDao userDao, JwtUtils jwtUtils, JavaMailSender mailSender) {
         this.userDao = userDao;
         this.jwtUtils = jwtUtils;
+        this.mailSender = mailSender;
     }
 
     @Override
@@ -31,7 +38,7 @@ public class AuthServiceImpl implements AuthService {
         String password = request.getPassword();
 
         UserDto userDto = userDao.findByUsernameOrEmail(login)
-                .orElseThrow(() -> new UnauthorizedException("User not found"));
+            .orElseThrow(() -> new UnauthorizedException("User not found"));
 
         if (!PasswordEncoder.matches(password, userDto.getPassword())) {
             throw new UnauthorizedException("Invalid password");
@@ -42,5 +49,15 @@ public class AuthServiceImpl implements AuthService {
 
         String token = jwtUtils.generateToken(user);
         return new AuthResponse(token, user);
+    }
+
+    @Async
+    protected void sendCodeToMail(String code, String receiver) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(SOURCE_EMAIL);
+        message.setSubject("Активация аккаунта");
+        message.setTo(receiver);
+        message.setText("Код для активации учетной записи - %s".formatted(code));
+        mailSender.send(message);
     }
 }

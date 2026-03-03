b98d2b728aa999e7bb7dc75bdf852f572c88dbbc
peremotka <ryabyhina.liza@mail.ru>
2026-03-03 19:27:22 +0300

    [TASK-048] Написала скрипт sql

--- server/migration/src/main/resources/migration/V2__user_profile_create.sql
+++ server/migration/src/main/resources/migration/V2__user_profile_create.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS profiles (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID NOT NULL UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+    id UUID PRIMARY KEY REFERENCES users(id),
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     company VARCHAR(255),
